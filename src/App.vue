783f902af18a18a0c77450a682d81879296a67e0
senlin <643620816@qq.com>
2019-04-12 18:23:19 +0800

    4-12 更新

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <span class="text">AVCDSDS</span>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  mounted(){
+    this.$store.dispatch('setTest',1)
   }
-}
+};
+document.addEventListener("DOMContentLoaded", () => {
+  let fontSize = window.innerWidth / 10;
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  const html = document.querySelector("html");
+  html.style.fontSize = fontSize + "px";
+});
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import "./assets/styles/global.scss";
+  // #app {
+  //   font-family: "Avenir", Helvetica, Arial, sans-serif;
+  //   -webkit-font-smoothing: antialiased;
+  //   -moz-osx-font-smoothing: grayscale;
+  //   text-align: center;
+  //   color: #2c3e50;
+  //   margin-top: 60px;
+  // }
+  
+  .text {
+    font-family: "Days One";
+    font-size: px2rem(20);
+    color: rebeccapurple;
+  }
 </style>
---
+++ src/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    test: 1
+  },
+  mutations: {
+    'SET_TEST': (state, newTest) => {
+      state.test = newTest
+    }
+  },
+  actions: {
+    setTest: ({commit,state}, newTest) => {
+      console.log(state.test,newTest)
+    }
+  }
+})
